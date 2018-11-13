0a44e7cf0bc224382c6a3025ff3bc108c4d7a148
roemerle64220 <44946294+roemerle64220@users.noreply.github.com>
2018-11-13 21:04:11 +0100

    Update ControllerUndo.java

--- adressverwaltung/controller/ControllerUndo.java
+++ adressverwaltung/controller/ControllerUndo.java
@@ -11,28 +11,48 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- *
- * @author Leonhard Römer
+ * class to implements the controller for the function undo()
+ * <br> in the class the event to invoke the undo()-command is registered and
+ * triggered if the specific event appears
+ * @author Leonhard Römer, Fabian Heise
  */
 public class ControllerUndo implements ActionListener {
-    
+    /**
+    * variable to declare the component view of the MVC-architecture
+    */
     private Surface view;
+    /**
+    * variable to declare the component model of the MVC-architecture
+    */
     private Application model;
+    /**
+    * variable to connect a collection of all avaiable commands to the controller
+    */
     private CommandInvoker invoker;
-    
+    /**
+    * Constructor to build the MVC-architecture and connect an invoker to the controller
+    * @param view set the component view of the MVC-architecture
+    * @param model set the component model of the MVC-architecture
+    * @param invoker connects an existing CommandInvoker to the controller
+    */
     public ControllerUndo(Surface view, Application model, CommandInvoker invoker)
     {
         this.view = view;
         this.model = model;
         this.invoker = invoker;
     }
-    
+    /**
+    * function to register the events within the controller
+    * <br>register the undo-Button to the controller
+    */
     public void registerEvents()
     {
         view.getMiUndo().addActionListener(this);
         
     }
-
+    /**
+    * function to identify an event and to call the related command
+    */
     @Override
     public void actionPerformed(ActionEvent e) {
         Object src = e.getSource();
